cd2bbd564b21c51f7681eb79631daa5335200a8a
Teesha Sohotoo <tsohotoo@gmail.com>
2025-03-06 23:37:49 +0400

    Fixed HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // ✅ Import HashRouter
+import { HashRouter } from 'react-router-dom'; // ✅ Fix for GitHub Pages
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>  {/* ✅ Wrap App in HashRouter */}
+    <HashRouter>
       <App />
     </HashRouter>
-  </StrictMode>,
+  </StrictMode>
 );
